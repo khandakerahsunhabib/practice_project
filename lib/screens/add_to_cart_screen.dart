6c35ac2ffee558299641821fe3494085453becb9
Khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2024-09-06 23:40:31 +0600

    Shopping Card design is done

--- lib/screens/add_to_cart_screen.dart
+++ lib/screens/add_to_cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_project/widgets/shopping_cart.dart';
 
 class AddToCartScreen extends StatefulWidget {
   const AddToCartScreen({super.key});
@@ -14,78 +15,14 @@ class _AddToCartScreenState extends State<AddToCartScreen> {
       appBar: AppBar(
         title: const Text('My Bag'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.teal.shade50,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    image: DecorationImage(
-                      image: AssetImage(''),
-                    ),
-                  ),
-                  child: Image.asset(
-                    'assets/images/shirt.jpg',
-                    width: 100,
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Pullover"),
-                      Row(
-                        children: [
-                          Text("Color: Black"),
-                          Text("Size: L"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.minimize,
-                              ),
-                            ),
-                          ),
-                          Text("1"),
-                          CircleAvatar(
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.add,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      Icon(
-                        Icons.menu,
-                      ),
-                      Text("51")
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            ShoppingCart(),
+            ShoppingCart(),
+            ShoppingCart(),
+          ],
+        ),
       ),
     );
   }
